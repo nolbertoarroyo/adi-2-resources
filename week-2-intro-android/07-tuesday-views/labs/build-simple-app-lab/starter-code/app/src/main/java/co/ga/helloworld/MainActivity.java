4f2dbaeaa9aaccb075f2797bc587caf5dfec667b
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-06-15 09:55:56 -0700

    added debug toast practice

--- week-2-intro-android/07-tuesday-views/labs/build-simple-app-lab/starter-code/app/src/main/java/co/ga/helloworld/MainActivity.java
+++ week-2-intro-android/07-tuesday-views/labs/build-simple-app-lab/starter-code/app/src/main/java/co/ga/helloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package co.ga.helloworld;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,65 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView colorText;
+    Button button1;
+    Button button2;
+    Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        colorText =  (TextView) findViewById(R.id.textView1);
+        button1 = (Button) findViewById(R.id.buttonOne);
+        button2 = (Button) findViewById(R.id.buttonTwo);
+        button3 = (Button) findViewById(R.id.buttonThree);
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                colorText.setTextColor(Color.BLUE);
+                button1.setTextColor(Color.BLUE);
+
+            }
+
+        };
+            button1.setOnClickListener(buttonListener);
+
+
+        View.OnClickListener buttonTwoListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                colorText.setTextColor(Color.GREEN);
+                button2.setTextColor(Color.GREEN);
+
+
+            }
+
+        };
+            button2.setOnClickListener(buttonTwoListener);
+
+        View.OnClickListener buttonThreeListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                colorText.setTextColor(Color.RED);
+                button3.setTextColor(Color.RED);
+
+
+            }
+
+        };
+            button3.setOnClickListener(buttonThreeListener);
+
+
     }
+
 }
+
--- week-2-intro-android/08-wednesday-activities/lessons/debugging-in-android-1/starter-code/app/src/main/java/co/ga/toasts/ToastActivity.java
+++ week-2-intro-android/08-wednesday-activities/lessons/debugging-in-android-1/starter-code/app/src/main/java/co/ga/toasts/ToastActivity.java
@@ -2,14 +2,61 @@ package co.ga.toasts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class ToastActivity extends AppCompatActivity {
+    Button buyButton;
+    Button saveButton;
+    Button sendButton;
+    Button likeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+
+        buyButton = (Button)findViewById(R.id.buyButton);
+        saveButton = (Button)findViewById(R.id.saveButton);
+        sendButton = (Button)findViewById(R.id.sendButton);
+        likeButton = (Button)findViewById(R.id.likeButton);
+
+        View.OnClickListener buyListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ToastActivity.this,"Buy button is pressed", Toast.LENGTH_SHORT).show();
+            }
+        };
+            buyButton.setOnClickListener(buyListener);
+
+        View.OnClickListener saveListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ToastActivity.this,"Save button is pressed", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+            saveButton.setOnClickListener(saveListener);
+
+        View.OnClickListener sendListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ToastActivity.this,"Send button is pressed", Toast.LENGTH_SHORT).show();
+            }
+        };
+            sendButton.setOnClickListener(sendListener);
+
+        View.OnClickListener likeListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ToastActivity.this,"LIke button is pressed", Toast.LENGTH_SHORT).show();
+                Log.v("ToastActivity", "LIKE BUTTON WAS CLICKED");
+            }
+        };
+        likeButton.setOnClickListener(likeListener);
     }
+
+
 }
