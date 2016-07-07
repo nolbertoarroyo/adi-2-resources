a03e16f13d5467963182e46aa9c5a14e553056c3
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-07 15:09:50 -0700

    lessons excercises

---
+++ week-5-fragments/24-thursday-multiple-devices/lessons/desiging=for-multiple-devices/MultipeDevices/app/src/main/java/com/test/multipedevices/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.multipedevices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = new Button(this);
+        String text = getString(R.string.button_text);
+        button.setText(text);
+    }
+}
