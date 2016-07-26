a0a054f0cdf3af1bfdde7204c0deac588b63d7bd
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-07-26 14:55:42 -0700

    bounded service lab

--- week-8-services/37-tuesday-recyclerview/labs/bound-services-lab/starter-code/BoundServicePractice/app/src/main/java/com/example/jeanweatherwax/boundservicepractice/MainActivity.java
+++ week-8-services/37-tuesday-recyclerview/labs/bound-services-lab/starter-code/BoundServicePractice/app/src/main/java/com/example/jeanweatherwax/boundservicepractice/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    private Boolean isBound=false;
+    BoundService myService;
 
     //create instance of your BoundService and set isBound to False
 
@@ -24,9 +27,22 @@ public class MainActivity extends Activity {
     // You should set isBound to the appropriate value in each method.
     // Yo should also set your binder in onServiceConnected.
     // Show a Toast in each.
+
     private ServiceConnection myConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            BoundService.MyLocalBinder binder = (BoundService.MyLocalBinder) iBinder ;
+            myService = binder.getService();
+            isBound = true;
+            Toast.makeText(MainActivity.this, "service binded",Toast.LENGTH_SHORT).show();
 
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound = false;
+            Toast.makeText(MainActivity.this, "service unbinded",Toast.LENGTH_SHORT).show();
+        }
     };
 
     @Override
@@ -41,6 +57,12 @@ public class MainActivity extends Activity {
         final Intent intent = new Intent(this, BoundService.class);
         bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
 
+        showTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTime(view);
+            }
+        });
 
         //set an onClickListened for the button to show the time
 
@@ -52,12 +74,18 @@ public class MainActivity extends Activity {
     @Override
     public void onStop() {
         super.onStop();
+        if (isBound) {
+            unbindService(myConnection);
+            isBound = false;
+        }
     }
 
 
     //in this method, get the current time using your service, and display it in the Textview.
     public void showTime(View view)
     {
-
+        String currentTime = myService.getCurrentTime();
+        TextView myTextView = (TextView)findViewById(R.id.myTextView);
+        myTextView.setText(currentTime);
     }
 }
